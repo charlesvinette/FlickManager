0a6475deb738412dadcb3e4c047570e23c13ccfc
Charles Vinette <vinettecharles@gmail.com>
2016-03-28 17:20:42 -0400

    Logic of the pod is in, need to test

---
+++ Example/Pods/WKLNFlickManagerViewController.swift
@@ -0,0 +1,162 @@
+//
+//  WKLNFlickManagerViewController.swift
+//  Pods
+//
+//  Created by Charles Vinette on 2016-03-28.
+//
+//
+
+import UIKit
+
+
+class FlickManager: UIViewController, UIGestureRecognizerDelegate {
+	
+	// MARK:Constants
+	let throwingThreshold: CGFloat = 1000
+	let throwingVelocityPadding: CGFloat = 10
+	
+	//MARK Variables
+	var leftButtonSize: Int = 120
+	var rightButtonSize: Int = 120
+	
+	var tap: UIGestureRecognizer!
+	var receivedView: UIView!
+	var animator: UIDynamicAnimator!
+	var originalBounds: CGRect!
+	var originalCenter: CGPoint!
+	var attachmentBehavior: UIAttachmentBehavior!
+	var push: UIPushBehavior!
+	var itemBehavior: UIDynamicItemBehavior!
+	var popupMovedVertically:Bool!
+	var leftButton:UIButton!
+	var rightButton:UIButton!
+	var didDragToLeft = false
+	var didDragToRight = false
+	
+	//MARK: Initialization
+	
+	init(view:UIView){
+		super.init(nibName: nil, bundle: nil)
+		self.view = view
+		self.createWithView(view)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func createWithView(view:UIView){
+		let animator = UIDynamicAnimator(referenceView: self.view)
+		self.animator = animator
+		self.originalBounds = self.view.bounds
+		self.originalCenter = self.view.center
+		
+		let panGesture = UIPanGestureRecognizer()
+		panGesture.addTarget(self, action: "handleAttachmentGesture:")
+		self.view.addGestureRecognizer(panGesture)
+		
+		//Right and Left Button
+		
+		let leftButton = UIButton()
+		self.leftButton = leftButton
+		leftButton.alpha = 0
+		self.view.addSubview(leftButton)
+		
+		let rightButton = UIButton()
+		self.rightButton = rightButton
+		self.view.addSubview(rightButton)
+	}
+	
+	
+	func handleAttachmentGesture(panGesture: UIPanGestureRecognizer) {
+		let location = panGesture.locationInView(self.view)
+		let boxLocation = panGesture.locationInView(self.view)
+		
+		let initialMidX = CGRectGetMidX(self.view.bounds)
+		
+		if panGesture.state == .Began {
+			
+			self.animator.removeAllBehaviors()
+			let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(self.view.bounds), boxLocation.y - CGRectGetMidY(self.view.bounds))
+			self.attachmentBehavior = UIAttachmentBehavior(item: self.view, offsetFromCenter: centerOffset, attachedToAnchor: location)
+			self.animator.addBehavior(attachmentBehavior)
+		}
+		
+		if panGesture.state == .Changed{
+			
+			if self.view.frame.midX > initialMidX + 150{
+				//Right
+				self.leftButton.alpha = (self.view.frame.midX - (initialMidX + 150)) * 0.03
+				if self.leftButton.alpha >= 1{
+					self.leftButton.alpha = 1
+					self.didDragToRight = true
+				}
+			}else if self.view.frame.midX < initialMidX - 150{
+				//Left
+				self.rightButton.alpha = ((initialMidX - 150) - self.view.frame.midX) * 0.03
+				if self.rightButton.alpha >= 1{
+					self.rightButton.alpha = 1
+					self.didDragToLeft = true
+				}
+			}
+		}
+		
+		if panGesture.state == .Ended {
+			
+			if self.view.frame.midX > initialMidX + 100{
+				//Right
+				popupMovedVertically = false
+				
+			}else if self.view.frame.midX < initialMidX - 100{
+				//Left
+				popupMovedVertically = false
+			}else{
+				popupMovedVertically = true
+			}
+			
+			
+			UIView.animateWithDuration(0.3, animations: {
+				self.leftButton.alpha = 0
+				self.rightButton.alpha = 0
+			})
+			self.animator.removeAllBehaviors()
+			
+			let velocity = panGesture.velocityInView(self.view)
+			let magnitude: CGFloat = sqrt((CGFloat(velocity.x * velocity.x) + CGFloat(velocity.y * velocity.y)))
+			
+			if magnitude > throwingThreshold && popupMovedVertically  {
+				let push = UIPushBehavior(items: [self.view], mode: .Instantaneous)
+				push.pushDirection = CGVectorMake((velocity.x / 10), (velocity.y / 10))
+				push.magnitude = magnitude / throwingVelocityPadding
+				self.push = push
+				self.animator.addBehavior(push)
+				//				self.performSelector("recenterView", withObject: nil, afterDelay: 0.4)
+				
+				self.dismissViewControllerAnimated(true, completion: nil)
+			} else {
+				//				AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+				self.recenterView()
+				if didDragToLeft{
+					didDragToLeft = false
+					rightButton.sendActionsForControlEvents(.TouchUpInside)
+				}
+				if didDragToRight{
+					didDragToRight = false
+					leftButton.sendActionsForControlEvents(.TouchUpInside)
+				}
+			}
+		} else {
+			self.attachmentBehavior.anchorPoint = panGesture.locationInView(self.view)
+		}
+	}
+	
+	func recenterView() {
+		self.animator.removeAllBehaviors()
+		UIView.animateWithDuration(0.5) { () -> Void in
+			
+			self.view.bounds = self.view.bounds
+			self.view.center = self.view.center
+			self.view.transform = CGAffineTransformIdentity
+		}
+	}
+}
